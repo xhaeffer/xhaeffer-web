37be6105bc4862543a66a4b39caf96f2340924b3
xhaeffer <subhansyahsp@gmail.com>
2025-03-01 19:03:17 +0700

    configure project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
+const flowbite = require("flowbite-react/tailwind");
+
+/*eslint-env node*/
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite/**/*.js",
+    flowbite.content(),
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [flowbite.plugin()],
+  darkMode: "class",
+};
