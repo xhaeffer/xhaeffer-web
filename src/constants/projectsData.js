47eaf2c62df2e834ac5f46c1987bcf4cd634c0f4
xhaeffer <subhansyahsp@gmail.com>
2025-03-05 14:36:57 +0700

    edit project data

--- src/constants/projectsData.js
+++ src/constants/projectsData.js
@@ -1,13 +1,14 @@
 const projectsData = [
   {
     id: 1,
-    title: "Point!",
+    title: "BajoBliss",
     category: "Website",
     cover:
-      "https://media.discordapp.net/attachments/1344970245374349342/1345002182100910141/image.png?ex=67c2f665&is=67c1a4e5&hm=84e9f8c8ae74230a787545fabee120f4a227caea3128ad7352c76e75cb159e31&=&format=webp&quality=lossless&width=1440&height=674",
-    type: "Front-End",
+      "https://i.ibb.co.com/6JHZxVT2/349894153-adb5110b-efdd-4058-86ba-8bb160f35cdf.png",
+    type: "Front-End & Back-End",
+    summary: "Capstone Project: BajoBliss - Baparekraf Digital Talent 2024",
     description:
-      "Final Submission: Menjadi Front-End Web Developer Expert - Dicoding Indonesia",
+      "https://raw.githubusercontent.com/BajoBliss/front/master/README.md",
     tags: [
       "HTML",
       "CSS",
@@ -18,31 +19,38 @@ const projectsData = [
       "Automation Testing",
     ],
     gallery: [
-      "https://media.discordapp.net/attachments/1344970245374349342/1344972997303472128/2025-02-28-17-01-dicoding-point-restaurant.vercel.app.png?ex=67c2db36&is=67c189b6&hm=0fe9295114505c33bbd9f7c7f9aaa86570b24254974c730d7ef1f48ceb36892a&=&format=webp&quality=lossless&width=631&height=678",
-      "https://media.discordapp.net/attachments/1344970245374349342/1344973014243999804/2025-02-28-16-59-dicoding-point-restaurant.vercel.app.png?ex=67c2db3a&is=67c189ba&hm=a63c163cbb6821ee00ab2260bd30d1538f91959e855f884c7f10670d43bc9206&=&format=webp&quality=lossless&width=470&height=678",
-      "https://media.discordapp.net/attachments/1344970245374349342/1345006924239998986/2025-02-28-19-15-dicoding-point-restaurant.vercel.app.png?ex=67c2facf&is=67c1a94f&hm=8e95b302f194ddcf8f08db7501f967d53c5b0a216608acfe5a1c51bed67e3104&=&format=webp&quality=lossless&width=470&height=678",
-      "https://media.discordapp.net/attachments/1344970245374349342/1344986085184372787/download_1.png?ex=67c2e767&is=67c195e7&hm=069398f383e4b5ba82cc2aee4b5e6b6d52912f60a9488bb400f7e4829cfea422&=&format=webp&quality=lossless&width=815&height=678",
-      "https://media.discordapp.net/attachments/1344970245374349342/1344986356325154896/image.png?ex=67c2e7a7&is=67c19627&hm=3e485b95acb6cc225ffe0c55b7d468863a87c2571568fb7362d3c95425de9e2a&=&format=webp&quality=lossless&width=1438&height=678",
+      "https://i.ibb.co.com/6JHZxVT2/349894153-adb5110b-efdd-4058-86ba-8bb160f35cdf.png",
+      "https://i.ibb.co.com/ccZ8hhSC/349894823-25501398-4b61-4ceb-9757-3c58f9829ffe.png",
+      "https://i.ibb.co.com/vC6XGfxZ/349894932-ae11a9be-a801-4303-9dda-28e7cadd7d13.png",
+      "https://i.ibb.co.com/1fN8Kp1V/349894693-dc088202-6cd6-41c7-854a-f36f13946541.png",
+      "https://i.ibb.co.com/ccZ8hhSC/349894823-25501398-4b61-4ceb-9757-3c58f9829ffe.png",
     ],
     additional: [
       {
-        title: "Github",
-        link: "https://github.com/xhaeffer/Dicoding-Point-Web",
+        title: "Github (Old)",
+        link: "https://github.com/lussyanast/bajobliss/tree/main",
+      },
+      {
+        title: "Github (New)",
+        link: "https://github.com/orgs/BajoBliss",
       },
       {
         title: "Live View",
-        link: "https://dicoding-point-restaurant.vercel.app/",
+        link: "#",
+        disabled: true,
       },
     ],
   },
   {
     id: 2,
-    title: "BajoBliss",
+    title: "Point!",
     category: "Website",
-    cover:
-      "https://media.discordapp.net/attachments/1344970245374349342/1345019063729918035/image.png?ex=67c3061e&is=67c1b49e&hm=f1979a04ce383bba9eb9ee713c11fe1224c8488e5a4d0ab8c2263e5e61a9105d&=&format=webp&quality=lossless&width=1440&height=664",
+    cover: "https://i.ibb.co.com/6RbLtHXc/bbbbbbb.png",
     type: "Front-End",
-    description: "Capstone Project: xxxx - Baparekraf Digital Talent 2024",
+    summary:
+      "Final Submission: Menjadi Front-End Web Developer Expert - Dicoding Indonesia",
+    description:
+      "https://raw.githubusercontent.com/xhaeffer/Dicoding-Point-Web/master/README.md",
     tags: [
       "HTML",
       "CSS",
@@ -52,7 +60,13 @@ const projectsData = [
       "Clean Code",
       "Automation Testing",
     ],
-    gallery: [],
+    gallery: [
+      "https://i.ibb.co.com/yJGQhYK/2025-02-28-17-01-dicoding-point-restaurant-vercel-app.png",
+      "https://i.ibb.co.com/SwpkSNqc/2025-02-28-16-59-dicoding-point-restaurant-vercel-app.png",
+      "https://i.ibb.co.com/yJGQhYK/2025-02-28-17-01-dicoding-point-restaurant-vercel-app.png",
+      "https://i.ibb.co.com/Ld4Dpz8F/2025-02-28-19-15-dicoding-point-restaurant-vercel-app.png",
+      "https://i.ibb.co.com/LzZvLC5V/aaaaa.png",
+    ],
     additional: [
       {
         title: "Github",
